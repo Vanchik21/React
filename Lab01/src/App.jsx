ffeb2bc2e7eac28302b31c342ec56d1943d3fd7c
Vanchik21 <ipz221_zia@student.ztu.edu.ua>
2024-12-10 02:32:52 +0200

    adapted lab 1 for lab 2

--- Lab01/src/App.jsx
+++ Lab01/src/App.jsx
@@ -1,7 +1,13 @@
+import LocationHistory from './components/LocationHistory'
 import Map from './components/Map'
 
 function App() {
-	return <Map />
+	return (
+		<>
+			<Map />
+			<LocationHistory />
+		</>
+	)
 }
 
 export default App
